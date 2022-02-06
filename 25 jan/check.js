c8865b7c562b6f97a3dc81ee9296b56c0a9b7f7f
rvrkr333 <roddamrushi@gmail.com>
2022-02-06 19:36:48 +0530

    first commit

---
+++ 25 jan/check.js
@@ -0,0 +1,7 @@
+var num = 10;
+if (num >= 10){
+    console.log(num + " is greater than or equal to  10");
+}
+else{
+    console.log(num + " is less than 10");
+} 
---
+++ 25 jan/greatest.js
@@ -0,0 +1,14 @@
+var a = parseInt(prompt("enter value of a "));
+var b = parseInt(prompt("enter value of b"));
+var c = parseInt(prompt("enter value of c"));
+
+
+if(a>=b && a>=c){
+    alert("greates of three numbers  is a = " + a );
+    }
+else if(b>=a && b>=c){
+    alert("greates of three numbers  is b = " + b);
+}
+else{
+    alert("greates of three numbers  is c =" + c);
+}
